a1aeb22963f165c35478cb069a346cbcfc64faa4
Валерий Воронов <valeravoronov88@gmail.com>
2025-01-28 15:13:16 +0300

    Выполнение проекта backend онлайн магазина

---
+++ online_store/admin/urls.py
@@ -0,0 +1,19 @@
+"""
+URL configuration for project.
+"""
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", include("frontend.urls")),
+    path('api/', include('backend.urls')),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns.extend(
+        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    )
